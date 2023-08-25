121a2538859e08946f841e1f9c38ee0069d3719b
Nwad-Rob <rnwadinobi@gmail.com>
2023-08-25 09:12:24 -0500

    Added SQl Connection

--- Activity/Util/DataConnect.java
+++ Activity/Util/DataConnect.java
@@ -1,5 +1,29 @@
 package Activity.Util;
 
+import java.sql.*;
+
 public class DataConnect {
-    
+  private static Connection con;
+
+  private DataConnect(){
+     try {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Shopee","root","Bigchill@510");
+        System.out.println("Data Connected");
+
+     } catch (ClassNotFoundException ex) {
+        System.out.println(ex.getMessage());
+        ex.printStackTrace();
+
+     }catch(SQLException ex){
+        System.out.println(ex.getMessage());
+        ex.printStackTrace();
+     }
+  }   
+
+  public static Connection getConnect(){
+    DataConnect d1 = new DataConnect();
+    return con;
+  }
+
 }
